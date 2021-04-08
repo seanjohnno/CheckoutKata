0bb172d9f2d04e6113b64300d3b13bfdaf017102
Sean Johnson <s3anj@hotmail.co.uk>
2021-04-08 21:28:59 +0100

    Test scanning multiple items

--- Source/CheckoutTests/CheckoutShould.cs
+++ Source/CheckoutTests/CheckoutShould.cs
@@ -19,11 +19,14 @@ namespace CheckoutTests
         public void ScanItems()
         {
             _checkout.ScanProduct("A99");
+            _checkout.ScanProduct("B15");
+            _checkout.ScanProduct("C40");
+            _checkout.ScanProduct("A99");
 
-            var scannedProduct = _checkout.ScannedProductList.First();
-
-            scannedProduct.SKU.Should().Be("A99");
-            scannedProduct.UnitPrice.Should().Be(50);
+            _checkout.ScannedProductList.Count.Should().Be(4);
+            _checkout.ScannedProductList.Single(p => p.SKU == "B15" && p.UnitPrice == 30);
+            _checkout.ScannedProductList.Single(p => p.SKU == "C40" && p.UnitPrice == 60);
+            _checkout.ScannedProductList.Where(p => p.SKU == "A99" && p.UnitPrice == 50).ToList().Count.Should().Be(2);
         }
     }
 }
