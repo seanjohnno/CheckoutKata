71374661197c5cca4d304675e28f1454fc28e95f
Sean Johnson <s3anj@hotmail.co.uk>
2021-04-08 20:58:55 +0100

    Creating skeleton project(s)

---
+++ Source/CheckoutTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Checkout;
+using Xunit;
+
+namespace CheckoutTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var testDeps = new Class1();
+        }
+    }
+}
